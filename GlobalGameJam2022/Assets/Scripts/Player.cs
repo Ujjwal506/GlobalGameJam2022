0301bbb32dda37e1e36a1dfcb8f4b0de854866a9
Ujjwal506 <77105552+Ujjwal506@users.noreply.github.com>
2022-01-29 13:54:40 +0530

    Fun....

---
+++ GlobalGameJam2022/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float lightSpeed = 5, darkSpeed = 3;
+    float speed;
+    Rigidbody2D rigibody;
+    SpriteRenderer spriteRenderer;
+    bool changeForm = true;
+    bool groundCheck = false;
+    void Awake()
+    {
+        rigibody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        ChangeForm();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(transform.right * -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if(groundCheck)
+                rigibody.velocity = new Vector3(0, speed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ChangeForm();
+        }
+    }
+    void ChangeForm() {
+        changeForm = !changeForm;
+        if (changeForm) { 
+            spriteRenderer.color = Color.black;
+            speed = darkSpeed;
+        }
+        else { 
+            spriteRenderer.color = Color.white;
+            speed = lightSpeed;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+            groundCheck = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+            groundCheck = false;
+    }
+}
